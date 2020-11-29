99a4698dab878495e5cf7b34abf8e66f4cf3235d
Jan Ossolinski <ossolinski.jan@gmail.com>
2020-11-29 00:04:04 +0000

    Add subpages and basic js functions

---
+++ js/settings.js
@@ -0,0 +1,32 @@
+//* global Handlebars */
+
+export const select = {
+  containerOf: {
+    pages: '#pages',
+  },
+  sidebar: {
+    menu: '.sidebar-menu',
+    content: '.sidebar-content',
+  },
+  nav: {
+    links: '.nav-list a',
+  },
+};
+
+export const classNames = {
+  sidebar: {
+    show: 'show',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+
+export const settings = {
+};
+
+export const templates = {
+};
