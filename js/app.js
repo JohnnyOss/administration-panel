92b1de847f89d246227845a91b80997b9f83a243
Jan Ossolinski <ossolinski.jan@gmail.com>
2020-12-04 19:14:40 +0000

    Add canvas graph

--- js/app.js
+++ js/app.js
@@ -311,6 +311,37 @@ const app = {
     payoutList();
   },
 
+  initGraph: function(){
+    const thisApp = this;
+    const graphContainer = document.getElementById('myChart').getContext('2d');
+    // eslint-disable-next-line no-undef
+    thisApp.graph = new Chart(graphContainer, {
+      type: 'bar',
+      data: {
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+        datasets: [{
+          label: 'Signups',
+          backgroundColor: '#8DBEC8',
+          borderColor: '#8DBEC8',
+          data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+          label: 'FTD',
+          backgroundColor: '#F29E4E',
+          borderColor: '#F29E4E',
+          data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+          label: 'Earned',
+          backgroundColor: '#71B374',
+          borderColor: '#71B374',
+          data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+          hidden: true,
+        }]
+      },
+    });
+  },
+
   sidebarShow: function(){
 
     function toggleMenu() {
@@ -385,6 +416,7 @@ const app = {
 
     thisApp.initLists();
     thisApp.initPages();
+    thisApp.initGraph();
     thisApp.sidebarShow();
     thisApp.popups();
   },
